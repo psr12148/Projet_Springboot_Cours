f9becaba6189516ffefefff8462e2b05647f038d
emile <psr12148@students.ephec.be>
2026-02-01 11:17:35 +0100

    initial commit

---
+++ src/main/java/com/example/springboot_cours/SpringbootCoursApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_cours/SpringbootCoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
