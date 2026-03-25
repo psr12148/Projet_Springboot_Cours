a304fc3c07a4ce89a7a9a88bd14d9c96fcf1ac6c
emile <psr12148@students.ephec.be>
2026-03-25 21:55:29 +0100

    Add springboot

---
+++ springbootCours2/src/main/java/be/ephec/pdw/springbootcours2/SpringbootCours2Application.java
@@ -0,0 +1,13 @@
+package be.ephec.pdw.springbootcours2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCours2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCours2Application.class, args);
+    }
+
+}
---
+++ springbootCours2/src/test/java/be/ephec/pdw/springbootcours2/SpringbootCours2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ephec.pdw.springbootcours2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCours2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
